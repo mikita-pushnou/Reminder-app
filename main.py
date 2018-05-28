63445a25cc28a1552f7104b7d91e1811ecee556e
mikita-pushnou <mikita.pushnou@gmail.com>
2018-05-28 23:31:45 +0300

    Update main.py
    Added: 
    label, button and popup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.config import Config
 
 
@@ -10,20 +11,35 @@ Config.set("graphics", "width", 400)
 Config.set("graphics", "height", 500)
 
 
-class ReminderApp(App):
-    def build(self):
+class myLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(myLayout, self).__init__(**kwargs)
 
         layout = BoxLayout(orientation="vertical",
                            padding=20,
                            spacing=10)
 
-        self.lbl = Label(text="Choose what to remind",
-                         font_size=50)
-        layout.add_widget(self.lbl)
-        layout.add_widget(Button(text="Option 1"))
-        layout.add_widget(Button(text="Option 2"))
+        btn = Button(text="Click")
+        btn.bind(on_press=self.clk)
 
-        return layout
+        lbl = Label(text="Lorem Ipsum")
+
+        self.add_widget(layout)
+        layout.add_widget(lbl)
+        layout.add_widget(btn)
+
+    def clk(self, obj):
+
+        popup = Popup(content=Label(text="I am popup"),
+                      size_hint=(None, None),
+                      size=(300, 200))
+
+        return popup.open()
+
+
+class ReminderApp(App):
+    def build(self):
+        return myLayout()
 
 
 if __name__ == "__main__":
