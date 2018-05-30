c641be309acc3131d3fb914227d98818e60bdcdc
mikita-pushnou <mikita.pushnou@gmail.com>
2018-05-31 00:21:47 +0300

    Update main.py
    Added:
    Working popup, timer

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.config import Config
-
+from threading import Timer
 
 Config.set("graphics", "resizable", 0)
 Config.set("graphics", "width", 400)
 Config.set("graphics", "height", 500)
 
+time_var = 5
+
 
 class myLayout(BoxLayout):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -19,22 +22,26 @@ class myLayout(BoxLayout):
                            padding=20,
                            spacing=10)
 
-        btn = Button(text="Click")
-        btn.bind(on_press=self.clk)
-
-        lbl = Label(text="Lorem Ipsum")
+        btn = Button(text="Set up timer")
 
         self.add_widget(layout)
-        layout.add_widget(lbl)
         layout.add_widget(btn)
 
-    def clk(self, obj):
+        btn.bind(on_press=self.timer)
 
-        popup = Popup(content=Label(text="I am popup"),
+    def popup(self):
+
+        popup = Popup(title="Remind you to",
+                      content=Label(text="go for a walk!"),
                       size_hint=(None, None),
-                      size=(300, 200))
+                      size=(350, 200))
 
         return popup.open()
+
+    def timer(self, obj):
+        timer = Timer(time_var, self.popup)
+
+        return timer.start()
 
 
 class ReminderApp(App):
